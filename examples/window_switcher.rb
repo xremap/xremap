ea34e7504ee8bfd21d0c960b286e8d7c95916f41
Takashi Kokubun <takashikkbn@gmail.com>
2017-01-09 02:10:56 +0900

    Improve window switcher example

--- examples/window_switcher.rb
+++ examples/window_switcher.rb
@@ -1,17 +1,8 @@
-define :activate do |command|
-  execute(<<-SHELL)
-    while read line; do
-      pid="$(echo "$line" | cut -d" " -f4)"
-      if [ "x#{command}" = "x$(cat "/proc/${pid}/cmdline")" ]; then
-        window_id="$(echo "$line" | cut -d" " -f1)"
-        exec wmctrl -i -R "$window_id"
-      fi
-    done <<< "$(wmctrl -l -p)"
-
-    exec "#{command}"
-  SHELL
+define :activate do |wm_class, command|
+  execute("wmctrl -x -a #{wm_class.shellescape} || #{command.shellescape}")
 end
 
-remap 'C-o', to: activate('/usr/share/nocturn/Nocturn')
-remap 'C-u', to: activate('/opt/google/chrome/chrome')
-remap 'C-h', to: activate('urxvt')
+# Check WM_CLASS by wmctrl -x -l
+remap 'C-o', to: activate('nocturn.Nocturn', '/usr/share/nocturn/Nocturn')
+remap 'C-u', to: activate('google-chrome.Google-chrome', '/opt/google/chrome/chrome')
+remap 'C-h', to: activate('urxvt.URxvt', 'urxvt')
