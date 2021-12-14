e29714484c45c146d88dc6ab6f44319a18203c21
Takashi Kokubun <takashikkbn@gmail.com>
2021-12-13 22:20:14 -0800

    Change the hard-coded device

--- src/input.rs
+++ src/input.rs
@@ -4,6 +4,6 @@ use evdev::Device;
 
 pub fn select_device() -> Device {
     // TODO: stop hard-coding the device
-    let device = Device::open("/dev/input/event2");
+    let device = Device::open("/dev/input/event19");
     return device.unwrap();
 }
