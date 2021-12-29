613555943d8e8037a3c0a508c1b20695d9a44817
Takashi Kokubun <takashikkbn@gmail.com>
2021-12-29 13:33:21 -0800

    Add serde test cases

--- src/config/tests.rs
+++ src/config/tests.rs
@@ -3,12 +3,107 @@ use indoc::indoc;
 use serde_yaml::Error;
 
 #[test]
-fn test_basic_remap() {
+fn test_modmap_basic() {
+    assert_parse(indoc! {"
+    modmap:
+      - name: Global
+        remap:
+          Alt_L: Ctrl_L
+      - remap:
+          Shift_R: Win_R
+        application:
+          only: Google-chrome
+    "})
+}
+
+#[test]
+fn test_modmap_application() {
+    assert_parse(indoc! {"
+    modmap:
+      - remap:
+          Alt_L: Ctrl_L
+        application:
+          not:
+            - Gnome-terminal
+      - remap:
+          Shift_R: Win_R
+        application:
+          only: Google-chrome
+    "})
+}
+
+#[test]
+fn test_modmap_multi_purpose_key() {
+    assert_parse(indoc! {"
+    modmap:
+      - remap:
+          Space:
+            held: Shift_L
+            alone: Space
+      - remap:
+          Muhenkan:
+            held: Alt_L
+            alone: Muhenkan
+            alone_timeout_millis: 500
+    "})
+}
+
+#[test]
+fn test_keymap_basic() {
     assert_parse(indoc! {"
     keymap:
       - name: Global
         remap:
           Alt-Enter: Ctrl-Enter
+      - remap:
+          Alt-S: Ctrl-S
+    "})
+}
+
+#[test]
+fn test_keymap_application() {
+    assert_parse(indoc! {"
+    keymap:
+      - remap:
+          Alt-Enter: Ctrl-Enter
+        application:
+          not: Gnome-terminal
+      - remap:
+          Alt-S: Ctrl-S
+        application:
+          only:
+            - Gnome-terminal
+    "})
+}
+
+#[test]
+#[ignore]
+fn test_keymap_array() {
+    assert_parse(indoc! {"
+    keymap:
+      - remap:
+          C-w:
+            - Shift-C-w
+            - C-x
+    "})
+}
+
+#[test]
+#[ignore]
+fn test_keymap_remap() {
+    assert_parse(indoc! {"
+    keymap:
+      - remap:
+          C-x:
+            remap:
+              s: C-w
+              C-s:
+                remap:
+                  x: C-z
+      - remap:
+          M-r:
+            - remap:
+              '1': C-t
     "})
 }
 
