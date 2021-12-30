21192ae2cee7a3a3615ed3564895a6af0594583d
Takashi Kokubun <takashikkbn@gmail.com>
2021-12-29 23:13:55 -0800

    Simplify Action deserializer

--- src/config/action.rs
+++ src/config/action.rs
@@ -2,76 +2,29 @@ use crate::config::key_press::KeyPress;
 use std::collections::HashMap;
 
 use serde::de;
-use serde::de::{IntoDeserializer, MapAccess, Visitor};
 use serde::{Deserialize, Deserializer};
-use std::fmt::{Debug, Formatter};
+use std::fmt::Debug;
 
 // Values in `keymap.remap`
-#[derive(Clone, Debug)]
+#[derive(Clone, Debug, Deserialize)]
+#[serde(untagged)]
 pub enum Action {
     KeyPress(KeyPress),
+    #[serde(deserialize_with = "deserialize_remap")]
     Remap(HashMap<KeyPress, Vec<Action>>),
 }
 
-impl<'de> Deserialize<'de> for Action {
-    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
-    where
-        D: Deserializer<'de>,
-    {
-        struct ActionVisitor;
-
-        impl<'de> Visitor<'de> for ActionVisitor {
-            type Value = Action;
-
-            fn expecting(&self, formatter: &mut Formatter) -> std::fmt::Result {
-                formatter.write_str("string or map")
-            }
-
-            fn visit_str<E>(self, value: &str) -> Result<Self::Value, E>
-            where
-                E: de::Error,
-            {
-                let key_press = Deserialize::deserialize(value.into_deserializer())?;
-                Ok(Action::KeyPress(key_press))
-            }
-
-            fn visit_map<M>(self, mut map: M) -> Result<Self::Value, M::Error>
-            where
-                M: MapAccess<'de>,
-            {
-                let key = map.next_key::<String>()?;
-                let action = match key.as_deref() {
-                    Some("remap") => {
-                        let mut action: HashMap<KeyPress, Vec<Action>> = HashMap::new();
-                        let remap = map.next_value::<HashMap<KeyPress, Actions>>()?;
-                        for (key_press, actions) in remap.into_iter() {
-                            action.insert(key_press, actions.to_vec());
-                        }
-                        Action::Remap(action)
-                    }
-                    Some(action) => return serde_error::<Self::Value, M>(&format!("unexpected action '{}'", action)),
-                    None => return serde_error::<Self::Value, M>("missing action"),
-                };
-                if let Some(key) = map.next_key::<String>()? {
-                    return serde_error::<Self::Value, M>(&format!(
-                        "only one action key is expected but also got: {}",
-                        key
-                    ));
-                }
-                Ok(action)
-            }
-        }
-
-        deserializer.deserialize_any(ActionVisitor)
-    }
-}
-
-fn serde_error<'de, V, M>(message: &str) -> Result<V, M::Error>
+fn deserialize_remap<'de, D>(deserializer: D) -> Result<HashMap<KeyPress, Vec<Action>>, D::Error>
 where
-    M: MapAccess<'de>,
+    D: Deserializer<'de>,
 {
-    let error: Box<dyn std::error::Error> = message.into();
-    Err(error).map_err(de::Error::custom)
+    let mut action = HashMap::<String, HashMap<KeyPress, Actions>>::deserialize(deserializer)?;
+    if let Some(remap) = action.remove("remap") {
+        if action.is_empty() {
+            return Ok(remap.into_iter().map(|(k, v)| (k, v.to_vec())).collect());
+        }
+    }
+    Err(de::Error::custom("not a map with a single \"remap\" key"))
 }
 
 // Used only for deserializing Vec<Action>
