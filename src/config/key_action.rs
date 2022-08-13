788624b9533543471871b13bddc139950300565a
Takashi Kokubun <takashikkbn@gmail.com>
2022-08-13 13:38:15 -0700

    Support executing commands on press/release events

--- src/config/key_action.rs
+++ src/config/key_action.rs
@@ -1,8 +1,10 @@
 use crate::config::key::deserialize_key;
 use evdev::Key;
-use serde::Deserialize;
+use serde::{Deserialize, Deserializer};
 use serde_with::{serde_as, DurationMilliSeconds};
 use std::time::Duration;
+
+use super::action::{Action, Actions};
 
 // Values in `modmap.remap`
 #[derive(Clone, Debug, Deserialize)]
@@ -11,6 +13,7 @@ pub enum KeyAction {
     #[serde(deserialize_with = "deserialize_key")]
     Key(Key),
     MultiPurposeKey(MultiPurposeKey),
+    PressReleaseKey(PressReleaseKey),
 }
 
 #[serde_as]
@@ -23,6 +26,23 @@ pub struct MultiPurposeKey {
     #[serde_as(as = "DurationMilliSeconds")]
     #[serde(default = "default_alone_timeout", rename = "alone_timeout_millis")]
     pub alone_timeout: Duration,
+}
+
+#[serde_as]
+#[derive(Clone, Debug, Deserialize)]
+pub struct PressReleaseKey {
+    #[serde(deserialize_with = "deserialize_actions")]
+    pub press: Vec<Action>,
+    #[serde(deserialize_with = "deserialize_actions")]
+    pub release: Vec<Action>,
+}
+
+pub fn deserialize_actions<'de, D>(deserializer: D) -> Result<Vec<Action>, D::Error>
+where
+    D: Deserializer<'de>,
+{
+    let actions = Actions::deserialize(deserializer)?;
+    return Ok(actions.into_vec());
 }
 
 fn default_alone_timeout() -> Duration {
