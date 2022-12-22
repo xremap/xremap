e292e344b4871f596d26147148e571cebeba4cef
Zai-Chun Chen <ChenZaichun@gmail.com>
2022-12-23 01:15:38 +0800

    Feature hyprland (#216)
    * init support for hyprland with hyprland-rs
    
    * enable ci for feature* branches
    
    * cargo fmt the code

---
+++ src/client/hypr_client.rs
@@ -0,0 +1,26 @@
+use crate::client::Client;
+use hyprland::data::blocking::get_active_window;
+use serde_json::Value;
+pub struct HyprlandClient;
+
+impl HyprlandClient {
+    pub fn new() -> HyprlandClient {
+        HyprlandClient {}
+    }
+}
+
+impl Client for HyprlandClient {
+    fn supported(&mut self) -> bool {
+        true
+    }
+
+    fn current_application(&mut self) -> Option<String> {
+        if let Ok(win) = get_active_window() {
+            let s = serde_json::to_string(&win).ok()?;
+            let v: Value = serde_json::from_str(&s).ok()?;
+            let app = v["class"].as_str();
+            return Some(String::from(app.unwrap()));
+        }
+        None
+    }
+}
