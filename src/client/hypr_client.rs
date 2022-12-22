1a4055dbf7f065bd447fe2e524dee9f77c25432d
Chen, Zai-Chun <chenzaichun@gmail.com>
2022-12-22 17:18:08 +0800

    cargo fmt the code

--- src/client/hypr_client.rs
+++ src/client/hypr_client.rs
@@ -1,11 +1,11 @@
 use crate::client::Client;
 use hyprland::data::blocking::get_active_window;
-use serde_json::{Value};
+use serde_json::Value;
 pub struct HyprlandClient;
 
 impl HyprlandClient {
     pub fn new() -> HyprlandClient {
-        HyprlandClient { }
+        HyprlandClient {}
     }
 }
 
@@ -15,7 +15,6 @@ impl Client for HyprlandClient {
     }
 
     fn current_application(&mut self) -> Option<String> {
-
         if let Ok(win) = get_active_window() {
             let s = serde_json::to_string(&win).ok()?;
             let v: Value = serde_json::from_str(&s).ok()?;
