2b2b95fb87b91a292a21f7d340a3d206ac38a119
yavko <yavornkolev@gmail.com>
2023-02-18 22:08:26 -0800

    Update Hyprland-rs

--- src/client/hypr_client.rs
+++ src/client/hypr_client.rs
@@ -1,5 +1,5 @@
 use crate::client::Client;
-use hyprland::data::blocking::get_active_window;
+use hyprland::{data::Client as HyprClient, prelude::*};
 use serde_json::Value;
 pub struct HyprlandClient;
 
@@ -15,7 +15,7 @@ impl Client for HyprlandClient {
     }
 
     fn current_application(&mut self) -> Option<String> {
-        if let Ok(win) = get_active_window() {
+        if let Ok(win) = HyprClient::get_active() {
             let s = serde_json::to_string(&win).ok()?;
             let v: Value = serde_json::from_str(&s).ok()?;
             let app = v["class"].as_str();
