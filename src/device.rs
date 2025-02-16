0708eb3ab29c76c8dd90666cc5d228eced1c00ff
Takashi Kokubun <takashikkbn@gmail.com>
2025-02-16 10:13:41 -0800

    Revert "Fix static_mut_refs warning"
    This reverts commit 5fda99fc90b4bd77429f4ec18986f7a12d361433.

--- src/device.rs
+++ src/device.rs
@@ -6,7 +6,6 @@ use derive_where::derive_where;
 use evdev::uinput::{VirtualDevice, VirtualDeviceBuilder};
 use evdev::{AttributeSet, BusType, Device, FetchEventsSynced, InputId, Key, RelativeAxisType};
 use nix::sys::inotify::{AddWatchFlags, InitFlags, Inotify};
-use std::cell::RefCell;
 use std::collections::HashMap;
 use std::error::Error;
 use std::fs::read_dir;
@@ -38,9 +37,7 @@ static MOUSE_BTNS: [&str; 20] = [
     "BTN_TASK",
 ];
 
-thread_local! {
-    static DEVICE_NAME: RefCell<Option<String>> = RefCell::new(None);
-}
+static mut DEVICE_NAME: Option<String> = None;
 
 // Credit: https://github.com/mooz/xkeysnail/blob/bf3c93b4fe6efd42893db4e6588e5ef1c4909cfb/xkeysnail/output.py#L10-L32
 pub fn output_device(bus_type: Option<BusType>, enable_wheel: bool) -> Result<VirtualDevice, Box<dyn Error>> {
@@ -257,20 +254,18 @@ impl InputDevice {
         }))
     }
 
-    fn current_name() -> String {
-        DEVICE_NAME.with_borrow_mut(|value| {
-            if value.is_none() {
-                let device_name = if Self::has_device_name("xremap") {
-                    format!("xremap pid={}", process::id())
-                } else {
-                    "xremap".to_string()
-                };
-
-                (*value) = Some(device_name);
+    fn current_name() -> &'static str {
+        if unsafe { DEVICE_NAME.is_none() } {
+            let device_name = if Self::has_device_name("xremap") {
+                format!("xremap pid={}", process::id())
+            } else {
+                "xremap".to_string()
             };
-
-            value.clone().unwrap()
-        })
+            unsafe {
+                DEVICE_NAME = Some(device_name);
+            }
+        }
+        unsafe { DEVICE_NAME.as_ref() }.unwrap()
     }
 
     fn has_device_name(device_name: &str) -> bool {
