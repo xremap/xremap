1e87d240aa6806f01a2322659979eba4c345a444
Henri <building.campus652@passmail.net>
2026-01-26 13:53:39 +0100

    refactor: Remove KWinScripting trait in kde_client

--- src/client/kde_client.rs
+++ src/client/kde_client.rs
@@ -33,88 +33,79 @@ impl Drop for KwinScriptTempFile {
     }
 }
 
-trait KWinScripting {
-    fn load_script(&self, path: &Path) -> Result<i32>;
-    fn unload_script(&self) -> Result<bool>;
-    fn start_script(&self, script_obj_id: i32) -> Result<()>;
-    fn is_script_loaded(&self) -> Result<bool>;
+fn load_script(conn: &Connection, path: &Path) -> Result<i32> {
+    Ok(block_on(
+        conn.call_method(
+            Some("org.kde.KWin"),
+            "/Scripting",
+            Some("org.kde.kwin.Scripting"),
+            "loadScript",
+            // since OsStr does not implement zvariant::Type, the temp-path must be valid utf-8
+            &(
+                path.to_str()
+                    .ok_or(anyhow::format_err!("Temp-path must be valid utf-8"))?,
+                KWIN_SCRIPT_PLUGIN_NAME,
+            ),
+        ),
+    )?
+    .body()
+    .deserialize::<i32>()?)
 }
 
-impl KWinScripting for Connection {
-    fn load_script(&self, path: &Path) -> Result<i32> {
-        Ok(block_on(
-            self.call_method(
-                Some("org.kde.KWin"),
-                "/Scripting",
-                Some("org.kde.kwin.Scripting"),
-                "loadScript",
-                // since OsStr does not implement zvariant::Type, the temp-path must be valid utf-8
-                &(
-                    path.to_str()
-                        .ok_or(anyhow::format_err!("Temp-path must be valid utf-8"))?,
-                    KWIN_SCRIPT_PLUGIN_NAME,
-                ),
-            ),
-        )?
-        .body()
-        .deserialize::<i32>()?)
-    }
+fn unload_script(conn: &Connection) -> Result<bool> {
+    Ok(block_on(conn.call_method(
+        Some("org.kde.KWin"),
+        "/Scripting",
+        Some("org.kde.kwin.Scripting"),
+        "unloadScript",
+        &KWIN_SCRIPT_PLUGIN_NAME,
+    ))?
+    .body()
+    .deserialize::<bool>()?)
+}
 
-    fn unload_script(&self) -> Result<bool> {
-        Ok(block_on(self.call_method(
+fn start_script(conn: &Connection, script_obj_id: i32) -> Result<()> {
+    for script_obj_path_fn in [|id| format!("/{id}"), |id| format!("/Scripting/Script{id}")] {
+        if block_on(conn.call_method(
             Some("org.kde.KWin"),
-            "/Scripting",
-            Some("org.kde.kwin.Scripting"),
-            "unloadScript",
-            &KWIN_SCRIPT_PLUGIN_NAME,
-        ))?
-        .body()
-        .deserialize::<bool>()?)
-    }
-
-    fn start_script(&self, script_obj_id: i32) -> Result<()> {
-        for script_obj_path_fn in [|id| format!("/{id}"), |id| format!("/Scripting/Script{id}")] {
-            if block_on(self.call_method(
-                Some("org.kde.KWin"),
-                script_obj_path_fn(script_obj_id).as_str(),
-                Some("org.kde.kwin.Script"),
-                "run",
-                &(),
-            ))
-            .is_ok()
-            {
-                return Ok(());
-            }
+            script_obj_path_fn(script_obj_id).as_str(),
+            Some("org.kde.kwin.Script"),
+            "run",
+            &(),
+        ))
+        .is_ok()
+        {
+            return Ok(());
         }
-        Err(anyhow::format_err!("Could not start KWIN script."))
     }
+    Err(anyhow::format_err!("Could not start KWIN script."))
+}
 
-    fn is_script_loaded(&self) -> Result<bool> {
-        Ok(block_on(self.call_method(
-            Some("org.kde.KWin"),
-            "/Scripting",
-            Some("org.kde.kwin.Scripting"),
-            "isScriptLoaded",
-            &KWIN_SCRIPT_PLUGIN_NAME,
-        ))?
-        .body()
-        .deserialize::<bool>()?)
-    }
+fn is_script_loaded(conn: &Connection) -> Result<bool> {
+    Ok(block_on(conn.call_method(
+        Some("org.kde.KWin"),
+        "/Scripting",
+        Some("org.kde.kwin.Scripting"),
+        "isScriptLoaded",
+        &KWIN_SCRIPT_PLUGIN_NAME,
+    ))?
+    .body()
+    .deserialize::<bool>()?)
 }
 
 fn load_kwin_script() -> Result<()> {
-    let dbus = block_on(Connection::session())?;
-    if !dbus.is_script_loaded()? {
+    let conn = block_on(Connection::session())?;
+    if !is_script_loaded(&conn)? {
         let init_script = || {
             let temp_file_path = KwinScriptTempFile::new();
             std::fs::write(&temp_file_path.0, KWIN_SCRIPT)?;
-            let script_obj_id = dbus.load_script(&temp_file_path.0)?;
-            dbus.start_script(script_obj_id)?;
+            let script_obj_id = load_script(&conn, &temp_file_path.0)?;
+            start_script(&conn, script_obj_id)?;
             Ok(())
         };
         if let Err(err) = init_script() {
             debug!("Trying to unload kwin-script plugin ('{KWIN_SCRIPT_PLUGIN_NAME}').");
-            match dbus.unload_script() {
+            match unload_script(&conn, ) {
                 Err(err) => debug!("Error unloading plugin ('{err:?}'). It may still be loaded and could cause future runs of xremap to fail."),
                 Ok(unloaded) if unloaded => debug!("Successfully unloaded plugin."),
                 Ok(_) => debug!("Plugin was not loaded in the first place."),
