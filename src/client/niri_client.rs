b409be309160477b804d2f528cafb45a7f87312b
Henri <building.campus652@passmail.net>
2026-04-25 16:15:59 +0200

    Implement niri close_windows_by_app_class

--- src/client/niri_client.rs
+++ src/client/niri_client.rs
@@ -1,6 +1,8 @@
 use crate::client::{Client, WindowInfo};
 use anyhow::bail;
-use niri_ipc::{socket::Socket, Request, Response};
+use log::debug;
+use niri_ipc::socket::Socket;
+use niri_ipc::{Action, Request, Response, Window};
 
 /// Client for the Niri scrollable Wayland compositor.
 ///
@@ -10,14 +12,16 @@ use niri_ipc::{socket::Socket, Request, Response};
 ///
 /// Note: This implementation creates a new socket connection for each query.
 /// In the future, a persistent connection might be more efficient.
-pub struct NiriClient;
+pub struct NiriClient {
+    socket: Option<Socket>,
+}
 
 impl NiriClient {
     pub fn new() -> NiriClient {
-        NiriClient {}
+        NiriClient { socket: None }
     }
 
-    fn get_active_window() -> Option<niri_ipc::Window> {
+    fn get_active_window() -> Option<Window> {
         let mut socket = match Socket::connect() {
             Ok(socket) => socket,
             Err(_) => return None,
@@ -32,6 +36,43 @@ impl NiriClient {
         match response {
             Response::Windows(windows) => windows.into_iter().find(|w| w.is_focused),
             _ => None,
+        }
+    }
+
+    fn get_socket<'a>(&'a mut self) -> anyhow::Result<&'a mut Socket> {
+        if self.socket.is_none() {
+            self.socket = Some(Socket::connect()?);
+        }
+
+        let socket = self
+            .socket
+            .as_mut()
+            .ok_or_else(|| anyhow::format_err!("This cannot happen"))?;
+
+        Ok(socket)
+    }
+
+    fn send_command<'a>(&'a mut self, action: Action) -> anyhow::Result<()> {
+        let response = self
+            .get_socket()?
+            .send(Request::Action(action))?
+            .map_err(|err| anyhow::format_err!("Niri failed: {err:?}"))?;
+
+        match response {
+            Response::Handled => Ok(()),
+            _ => bail!("Niri sent unexpected response."),
+        }
+    }
+
+    fn get_windows(&mut self) -> anyhow::Result<Vec<Window>> {
+        let windows = self
+            .get_socket()?
+            .send(Request::Windows)?
+            .map_err(|err| anyhow::format_err!("Niri failed: {err:?}"))?;
+
+        match windows {
+            Response::Windows(windows) => Ok(windows),
+            _ => bail!("Niri sent unexpected response."),
         }
     }
 }
@@ -70,7 +111,14 @@ impl Client for NiriClient {
         bail!("window_list not implemented for NIRI")
     }
 
-    fn close_windows_by_app_class(&mut self, _app_class: &str) -> anyhow::Result<()> {
-        bail!("close_windows_by_app_class not implemented for NIRI")
+    fn close_windows_by_app_class(&mut self, app_class: &str) -> anyhow::Result<()> {
+        for window in self.get_windows()? {
+            if window.app_id.as_deref() == Some(app_class) {
+                debug!("Closing: {:?}", window.title);
+                self.send_command(Action::CloseWindow { id: Some(window.id) })?;
+            }
+        }
+
+        Ok(())
     }
 }
