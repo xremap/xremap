98700228638bd64ddcd9d13ca6e183763299d3da
Henri <building.campus652@passmail.net>
2026-06-01 18:29:31 +0200

    refactor: Nested remap can't match anykey.

--- src/event_handler.rs
+++ src/event_handler.rs
@@ -474,48 +474,48 @@ impl EventHandler {
         device: &InputDeviceInfo,
         wmclient: &mut WMClient,
     ) -> Result<Option<Vec<TaggedAction>>, Box<dyn Error>> {
-        for key in [key, &KEY_MATCH_ANY] {
-            if !self.override_remaps.is_empty() {
-                let entries: Vec<OverrideEntry> = self
-                    .override_remaps
-                    .iter()
-                    .flat_map(|map| map.get(key).cloned().unwrap_or_default())
-                    .collect();
+        if !self.override_remaps.is_empty() {
+            let entries: Vec<OverrideEntry> = self
+                .override_remaps
+                .iter()
+                .flat_map(|map| map.get(key).cloned().unwrap_or_default())
+                .collect();
 
-                // Empty if the key isn't defined in any of the nested remaps, that are active.
-                if !entries.is_empty() {
-                    self.remove_override()?;
+            // Empty if the key isn't defined in any of the nested remaps, that are active.
+            if !entries.is_empty() {
+                self.remove_override()?;
 
-                    for exact_match in [true, false] {
-                        let mut remaps = vec![];
-                        for entry in &entries {
-                            if entry.exact_match && !exact_match {
-                                continue;
-                            }
-                            let (extra_modifiers, missing_modifiers) = self.diff_modifiers(&entry.modifiers);
-                            if (exact_match && !extra_modifiers.is_empty()) || !missing_modifiers.is_empty() {
-                                continue;
-                            }
-
-                            let actions = with_extra_modifiers(&entry.actions, &extra_modifiers, entry.exact_match);
-                            let has_remap = has_remap(&entry.actions);
-
-                            // If the first/top match was a remap, continue to find rest of the eligible remaps for this key
-                            if remaps.is_empty() && !has_remap {
-                                return Ok(Some(actions));
-                            } else if has_remap {
-                                remaps.extend(actions);
-                            }
+                for exact_match in [true, false] {
+                    let mut remaps = vec![];
+                    for entry in &entries {
+                        if entry.exact_match && !exact_match {
+                            continue;
                         }
-                        if !remaps.is_empty() {
-                            return Ok(Some(remaps));
+                        let (extra_modifiers, missing_modifiers) = self.diff_modifiers(&entry.modifiers);
+                        if (exact_match && !extra_modifiers.is_empty()) || !missing_modifiers.is_empty() {
+                            continue;
+                        }
+
+                        let actions = with_extra_modifiers(&entry.actions, &extra_modifiers, entry.exact_match);
+                        let has_remap = has_remap(&entry.actions);
+
+                        // If the first/top match was a remap, continue to find rest of the eligible remaps for this key
+                        if remaps.is_empty() && !has_remap {
+                            return Ok(Some(actions));
+                        } else if has_remap {
+                            remaps.extend(actions);
                         }
                     }
+                    if !remaps.is_empty() {
+                        return Ok(Some(remaps));
+                    }
                 }
-                // An override remap is set but not used. Flush the pending key.
-                self.timeout_override()?;
             }
+            // An override remap is set but not used. Flush the pending key.
+            self.timeout_override()?;
+        }
 
+        for key in [key, &KEY_MATCH_ANY] {
             if let Some(entries) = config.keymap_table.get(key) {
                 for exact_match in [true, false] {
                     let mut remaps = vec![];
