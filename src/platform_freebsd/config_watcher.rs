2ac1eae2d5a2406bca0020100dfa131c5bf32c8c
Henri <building.campus652@passmail.net>
2026-08-02 18:35:30 +0200

    Fix freebsd compile errors

--- src/platform_freebsd/config_watcher.rs
+++ src/platform_freebsd/config_watcher.rs
@@ -1,21 +1,19 @@
-use crate::config::Config;
-use crate::main_controller::MainController;
+use crate::main_impl::MainAction;
 use anyhow::Result;
-use nix::sys::select::FdSet;
 use std::os::fd::RawFd;
 use std::path::PathBuf;
 
 pub struct ConfigWatcher {}
 
 impl ConfigWatcher {
-    pub fn new(watch: bool, _files: Vec<PathBuf>, _debounce_ms: u64, _notifications: bool) -> Result<Option<Self>> {
+    pub fn new(watch: bool, _files: Vec<PathBuf>, _debounce_ms: u64) -> Result<Option<Self>> {
         if watch {
             println!("Config watch is not supported on FreeBSD");
         }
         return Ok(None);
     }
 
-    pub fn handle(&mut self, _readable_fds: Vec<RawFd>, _mainctrl: &mut MainController) -> Result<Option<Config>> {
+    pub fn handle(&mut self, _readable_fds: Vec<RawFd>) -> Result<Option<MainAction>> {
         unreachable!()
     }
 }
