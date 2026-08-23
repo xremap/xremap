4c1104156fb1533e7d9c84021efeff01f6f912e6
Henri <building.campus652@passmail.net>
2026-08-23 08:42:07 +0200

    Remove serde_with crate

--- src/config/expmap_simkey.rs
+++ src/config/expmap_simkey.rs
@@ -1,19 +1,17 @@
 use crate::config::deserialize_keys;
+use crate::config::deserializers::deserialize_duration;
 use crate::config::expmap_operator::{deserialize_expmap_actions, ExpmapAction};
 use evdev::KeyCode as Key;
 use serde::Deserialize;
-use serde_with::{serde_as, DurationMilliSeconds};
 use std::time::Duration;
 
-#[serde_as]
 #[derive(Clone, Debug, Deserialize)]
 pub struct Simkey {
     #[serde(deserialize_with = "deserialize_keys")]
     pub keys: Vec<Key>,
     #[serde(deserialize_with = "deserialize_expmap_actions")]
     pub actions: Vec<ExpmapAction>,
-    #[serde_as(as = "DurationMilliSeconds")]
-    #[serde(default = "default_symkey_timeout")]
+    #[serde(default = "default_symkey_timeout", deserialize_with = "deserialize_duration")]
     pub timeout: Duration,
 }
 
